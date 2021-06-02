0f424497bcbb061f1e9b35d736dfac0a3bc5353b
Thomas Churchman <thomas@kepow.org>
2021-06-02 22:43:18 +0200

    Move stable to released nixos-21.05, bump lockfile

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tom's systems configurations";
 
   inputs = {
-    stable.url = "github:NixOS/nixpkgs/release-21.05";
+    stable.url = "github:NixOS/nixpkgs/nixos-21.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     patched.url = "path:./nixpkgs";
     home-manager = {
