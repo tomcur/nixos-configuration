47f351529b80d26fb1a9ddd360ecec13c342e5db
Thomas Churchman <thomas@kepow.org>
2021-11-03 14:26:11 +0100

    nvim: use telescope's fuzzy_file sorter

--- config/home/cfg/nvim/default.nix
+++ config/home/cfg/nvim/default.nix
@@ -108,6 +108,7 @@ in
               file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
               grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
               qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+              file_sorter = require'telescope.sorters'.get_fuzzy_file,
             }
           }
           EOF
