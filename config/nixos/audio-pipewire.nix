32f56339c8a1e4cd286a2e14181c416545d14f6b
Thomas Churchman <thomas@kepow.org>
2022-02-18 17:19:46 +0100

    Improve pipewire config

--- config/nixos/audio-pipewire.nix
+++ config/nixos/audio-pipewire.nix
@@ -21,6 +21,8 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    media-session.enable = false;
+    wireplumber.enable = true;
     config.pipewire = {
       "context.properties" = {
         "default.clock.rate" = 44100;
@@ -35,23 +37,40 @@
       };
     };
     config.pipewire-pulse = {
-      # "context.modules" = [
-      #   {
-      #     name = "libpipewire-module-protocol-pulse";
-      #     args = {
-      #       "pulse.min.req" = "32/44100";
-      #       "pulse.default.req" = "32/44100";
-      #       "pulse.max.req" = "32/44100";
-      #       "pulse.min.quantum" = "32/44100";
-      #       "pulse.max.quantum" = "32/44100";
-      #       "server.address" = [ "unix:native" ];
-      #     };
-      #   }
-      # ];
-      # "stream.properties" = {
-      #   "node.latency" = "32/44100";
-      #   "resample.quality" = 1;
-      # };
+      "context.properties" = {
+        "log.level" = 2;
+      };
+      "context.modules" = [
+        {
+          name = "libpipewire-module-rtkit";
+          args = {
+            "nice.level" = -15;
+            "rt.prio" = 88;
+            "rt.time.soft" = 200000;
+            "rt.time.hard" = 200000;
+          };
+          flags = [ "ifexists" "nofail" ];
+        }
+        { name = "libpipewire-module-protocol-native"; }
+        { name = "libpipewire-module-client-node"; }
+        { name = "libpipewire-module-adapter"; }
+        { name = "libpipewire-module-metadata"; }
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            "pulse.min.req" = "32/44100";
+            "pulse.default.req" = "32/44100";
+            "pulse.max.req" = "32/44100";
+            "pulse.min.quantum" = "32/44100";
+            "pulse.max.quantum" = "32/44100";
+            "server.address" = [ "unix:native" "tcp:4713" ];
+          };
+        }
+      ];
+      "stream.properties" = {
+        "node.latency" = "32/44100";
+        "resample.quality" = 1;
+      };
     };
   };
 }
