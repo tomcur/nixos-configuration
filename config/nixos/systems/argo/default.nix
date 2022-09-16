f5506820dfbe1ba455282eaf75ffbd7d417ba97e
Thomas Churchman <thomas@kepow.org>
2022-09-16 12:10:05 +0200

    Remove argo

--- config/nixos/systems/argo/default.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  imports = [
-    # TODO: copy this file over from the machine
-    ./hardware-configuration.nix
-    ./configuration.nix
-  ];
-}
--- flake.nix
+++ flake.nix
@@ -101,40 +101,6 @@
           lib.nixosSystem {
             inherit system specialArgs modules;
           };
-        argo =
-          let
-            system = "x86_64-linux";
-            specialArgs = {
-              inherit inputs system;
-              stablePkgs = import stable { inherit system; config = { allowUnfree = true; }; };
-              unstablePkgs = import unstable { inherit system; config = { allowUnfree = true; }; };
-              patchedPkgs = import patched { inherit system; config = { allowUnfree = true; }; };
-              neovimPkg = neovim.defaultPackage.${system};
-              neovimPlugins = neovim.plugins.${system};
-              awesomePkg = awesome.defaultPackage.${system};
-              awesomePlugins = awesome.plugins.${system};
-              deployrsPkgs = inputs.deploy-rs.${system}.packages;
-            };
-            modules = [
-              {
-                nixpkgs.overlays = [ ];
-                nixpkgs.config.allowUnfree = true;
-              }
-              (import ./config/nixos/systems/argo)
-              inputs.home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.extraSpecialArgs = specialArgs;
-                home-manager.users.thomas = import ./config/home/systems/argo/default.nix;
-              }
-              nixos-hardware.nixosModules.dell-xps-15-7590
-              unstable.nixosModules.notDetected
-            ];
-          in
-          lib.nixosSystem {
-            inherit system specialArgs modules;
-          };
         pollux =
           let
             system = "x86_64-linux";
