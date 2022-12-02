76f555b0745f404f415d75ab7a29001fd64eede1
Thomas Churchman <thomas@kepow.org>
2022-12-02 01:17:50 +0100

    castor: move back from Mopidy to MPD

--- config/nixos/systems/castor/music.nix
+++ config/nixos/systems/castor/music.nix
@@ -1,35 +1,35 @@
 { config, patchedPkgs, pkgs, ... }: {
-  imports = [ ./mop.nix ];
-  disabledModules = [ "services/audio/mopidy.nix" ];
-  services.mopidy = {
-    enable = true;
-    extensionPackages = [
-      pkgs.mopidy-mpd
-      pkgs.mopidy-local
-      pkgs.mopidy-iris
-    ];
-    configuration = ''
-      [audio]
-      output = pulsesink server=127.0.0.1:4713
+  # imports = [ ./mop.nix ];
+  # disabledModules = [ "services/audio/mopidy.nix" ];
+  # services.mopidy = {
+  #   enable = true;
+  #   extensionPackages = [
+  #     pkgs.mopidy-mpd
+  #     pkgs.mopidy-local
+  #     pkgs.mopidy-iris
+  #   ];
+  #   configuration = ''
+  #     [audio]
+  #     output = pulsesink server=127.0.0.1:4713
 
-      [mpd]
-      enabled = true
-      hostname = ::
+  #     [mpd]
+  #     enabled = true
+  #     hostname = ::
 
-      [iris]
-      enabled = true
-      country = NL
-      locale = en_EN
-      snapcast_enabled = false
+  #     [iris]
+  #     enabled = true
+  #     country = NL
+  #     locale = en_EN
+  #     snapcast_enabled = false
 
-      [file]
-      enabled = false
+  #     [file]
+  #     enabled = false
 
-      [local]
-      enabled = true
-      media_dir =
-        /home/srv/music/artists
-      scan_follow_symlinks = true
-    '';
-  };
+  #     [local]
+  #     enabled = true
+  #     media_dir =
+  #       /home/srv/music/artists
+  #     scan_follow_symlinks = true
+  #   '';
+  # };
 }
