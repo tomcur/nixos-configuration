af6364b878e6dc295d51d9a6b3d1437f43a3a6ae
Thomas Churchman <thomas@kepow.org>
2023-11-07 14:17:20 +0100

    rofi: use pkgs instead of unstablePkgs

--- config/home/cfg/rofi/default.nix
+++ config/home/cfg/rofi/default.nix
@@ -1,7 +1,7 @@
-{ unstablePkgs, ... }:
+{ pkgs, ... }:
 {
   home.packages = [
-    unstablePkgs.rofi
+    pkgs.rofi
   ];
   xdg.configFile."rofi/config.rasi".source = ./config.rasi;
   xdg.configFile."rofi/theme.rasi".source = ./theme.rasi;
