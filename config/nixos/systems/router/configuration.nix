728daf47703d67cb2bf041251f2c26b863d8f034
Thomas Churchman <thomas@kepow.org>
2024-02-24 10:54:06 +0100

    router: allow jumbo frames on LAN interface

--- config/nixos/systems/router/configuration.nix
+++ config/nixos/systems/router/configuration.nix
@@ -112,6 +112,9 @@ in
       };
       "30-lan" = {
         matchConfig.Name = if_lan;
+        linkConfig = {
+          MTUBytes = "9000";
+        };
         address = [
           "10.0.0.1/16"
         ];
