6af708cafebe406e65716026e6a058f9e979adf6
Thomas Churchman <thomas@kepow.org>
2024-06-03 11:18:47 +0200

    Upgrade to NixOS 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tom's systems configurations";
 
   inputs = {
-    stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     patched.url = "git+file:///etc/nixos/nixpkgs";
